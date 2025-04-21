e80a1a458f18ace8ef5fead2216f7def20e2c66f
kira42310 <soratouch@hotmail.com>
2025-04-21 15:38:34 +0900

    init for pjsub

--- src/psij/executors/__init__.py
+++ src/psij/executors/__init__.py
@@ -4,11 +4,13 @@ from .local import LocalJobExecutor
 from .batch.cobalt import CobaltJobExecutor
 from .batch.lsf import LsfJobExecutor
 from .batch.slurm import SlurmJobExecutor
+from .batch.pjsub import PJsubJobExecutor
 
 
 __all__ = [
     'LocalJobExecutor',
     'LsfJobExecutor',
     'CobaltJobExecutor',
-    'SlurmJobExecutor'
+    'SlurmJobExecutor',
+    'PJsubJobExecutor'
 ]
