de497b68f0a6c39175be33a62b104e25f88d09d7
kira42310 <soratouch@hotmail.com>
2025-06-02 11:25:13 +0900

    Add error messages

--- src/psij/executors/batch/pjsub.py
+++ src/psij/executors/batch/pjsub.py
@@ -53,81 +53,38 @@ class PJsubJobExecutor(BatchSchedulerExecutor):
         'RUN': JobState.ACTIVE,
         'SPD': JobState.QUEUED,
         'SPP': JobState.QUEUED,
-        #'BF': JobState.FAILED,
-        #'CA': JobState.CANCELED,
-        #'CD': JobState.COMPLETED,
-        #'CF': JobState.QUEUED,
-        #'CG': JobState.ACTIVE,
-        #'DL': JobState.FAILED,
-        #'F': JobState.FAILED,
-        #'NF': JobState.FAILED,
-        #'OOM': JobState.FAILED,
-        #'PD': JobState.QUEUED,
-        #'PR': JobState.FAILED,
-        #'R': JobState.ACTIVE,
-        #'RD': JobState.QUEUED,
-        #'RF': JobState.QUEUED,
-        #'RH': JobState.QUEUED,
-        #'RQ': JobState.ACTIVE,
-        #'SO': JobState.ACTIVE,
-        #'TO': JobState.FAILED,
-        ## TODO: double-check these
-        #'RS': JobState.ACTIVE,
-        #'RV': JobState.QUEUED,
-        #'SI': JobState.ACTIVE,
-        #'SE': JobState.ACTIVE,
-        #'ST': JobState.ACTIVE,
-        #'S': JobState.ACTIVE
     }
 
     # see https://slurm.schedmd.com/squeue.html
     _REASONS_MAP = {
-        '-':'No error',
-        #'AssociationJobLimit': 'The job\'s association has reached its maximum job count.',
-        #'AssociationResourceLimit': 'The job\'s association has reached some resource limit.',
-        #'AssociationTimeLimit': 'The job\'s association has reached its time limit.',
-        #'BadConstraints': 'The job\'s constraints can not be satisfied.',
-        #'BeginTime': 'The job\'s earliest start time has not yet been reached.',
-        #'Cleaning': 'The job is being requeued and still cleaning up from its previous execution.',
-        #'Dependency': 'This job is waiting for a dependent job to complete.',
-        #'FrontEndDown': 'No front end node is available to execute this job.',
-        #'InactiveLimit': 'The job reached the system InactiveLimit.',
-        #'InvalidAccount': 'The job\'s account is invalid.',
-        #'InvalidQOS': 'The job\'s QOS is invalid.',
-        #'JobHeldAdmin': 'The job is held by a system administrator.',
-        #'JobHeldUser': 'The job is held by the user.',
-        #'JobLaunchFailure': 'The job could not be launched.This may be due to a file system '
-        #                    'problem, invalid program name, etc.',
-        #'Licenses': 'The job is waiting for a license.',
-        #'NodeDown': 'A node required by the job is down.',
-        #'NonZeroExitCode': 'The job terminated with a non-zero exit code.',
-        #'PartitionDown': 'The partition required by this job is in a DOWN state.',
-        #'PartitionInactive': 'The partition required by this job is in an Inactive state and not '
-        #                     'able to start jobs.',
-        #'PartitionNodeLimit': 'The number of nodes required by this job is outside of its '
-        #                      'partition\'s current limits. Can also indicate that required nodes '
-        #                      'are DOWN or DRAINED.',
-        #'PartitionTimeLimit': 'The job\'s time limit exceeds its partition\'s current time limit.',
-        #'Priority': 'One or more higher priority jobs exist for this partition or advanced '
-        #            'reservation.',
-        #'Prolog': 'Its PrologSlurmctld program is still running.',
-        #'QOSJobLimit': 'The job\'s QOS has reached its maximum job count.',
-        #'QOSResourceLimit': 'The job\'s QOS has reached some resource limit.',
-        #'QOSTimeLimit': 'The job\'s QOS has reached its time limit.',
-        #'ReqNodeNotAvail': 'Some node specifically required by the job is not currently available. '
-        #                   'The node may currently be in use, reserved for another job, in an '
-        #                   'advanced reservation, DOWN, DRAINED, or not responding. Nodes which '
-        #                   'are DOWN, DRAINED, or not responding will be identified as part of '
-        #                   'the job\'s "reason" field as "UnavailableNodes". Such nodes will '
-        #                   'typically require the intervention of a system administrator to make '
-        #                   'available.',
-        #'Reservation': 'The job is waiting its advanced reservation to become available.',
-        #'Resources': 'The job is waiting for resources to become available.',
-        #'SystemFailure': 'Failure of the Slurm system, a file system, the network, etc.',
-        #'TimeLimit': 'The job exhausted its time limit.',
-        #'QOSUsageThreshold': 'Required QOS threshold has been breached.',
-        #'WaitingForScheduling': 'No reason has been set for this job yet. Waiting for the '
-        #                        'scheduler to determine the appropriate reason.'
+        '':'No error',
+        'ANOTHER JOB STARTED': 'A job that was running beyond the minimum runnable time for the job has been terminated to run a subsequent job.',
+        'DEADLINE SCHEDULE STARTED': 'A job that was running beyond the minimum job execution time was terminated due to the start of the deadline schedule.',
+        'ELAPSE LIMIT EXCEEDED': 'The elapsed time limit has been exceeded.',
+        'FILE IO ERROR': 'The current directory when the user’s job is submitted cannot be accessed.',
+        'GATE CHECK': 'Canceled by the job manager exit function.',
+        'IMPOSSIBLE SCHED': 'Scheduling failed.',
+        'INSUFF CPU': 'There is a physical shortage of CPUs.',
+        'INSUFF MEMORY': 'There is a physical memory shortage.',
+        'INSUFF NODE': 'The number of nodes is physically insufficient.',
+        'INSUFF CustomResourceName': 'The custom resource defined by the resource name CustomResourceName is insufficient.',
+        'INTERNAL ERROR': 'Internal error.',
+        'INVALID HOSTFILE': 'The host file is unmatched which specified with rank-map-hostfile parameter of pjsub command.',
+        'LIMIT OVER MEMORY': 'The memory limit was exceeded during job execution.',
+        'LOST COMM': 'All-to-all communication of parallel processes is not guaranteed.',
+        'NO CURRENT DIR': 'The current directory or standard input / standard output / standard error output file when the user job was submitted could not be accessed.',
+        'NOT EXIST CustomResourceName': 'A custom resource with the resource name CustomResourceName is not defined.',
+        'RESUME FAIL': 'Resume failed.',
+        'RSCGRP NOT EXIST': 'Resource group does not exist.',
+        'RSCGRP STOP': 'The resource group has stopped.',
+        'RSCUNIT NOT EXIST': 'Resource unit does not exist.',
+        'RSCUNIT STOP': 'The resource unit has stopped.',
+        'RUNLIMIT EXCEED': 'The maximum number of concurrent job executions has been exceeded.',
+        'SUSPEND FAIL': 'Suspend failed.',
+        'USELIMIT EXCEED': 'Waiting for execution due to simultaneous node limit or concurrent CPU core limit.',
+        'USER NOT EXIST': 'The job execution user does not exist in the system.',
+        'WAIT SCHED': 'The number of jobs subject to scheduling has been reached, so it has been excluded from scheduling.',
+        # '': '',
     }
 
     def __init__(self, url: Optional[str] = None, config: Optional[PJsubExecutorConfig] = None):
